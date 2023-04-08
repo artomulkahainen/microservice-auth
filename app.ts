9f9a8293731bd7e2f9f403b7b08499249fbcb5ca
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2023-04-08 12:30:44 +0300

    Morgan added to app, package.json fixed

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
+import morgan from 'morgan';
 import routes from '@controllers/routes';
 
 dotenv.config();
 const app = express();
 
+morgan.token('body', (req) => {
+    return JSON.stringify(req);
+});
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms')
+);
 app.use(express.json());
 app.use('/api', routes);
 
